e3542b4e54344e2a3b389f94af331079b1ac184c
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2021-10-24 01:23:36 +0200

    YPW Adds year percentage progress bar compo (#3)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { getDaysInYear, getDayOfYear } from "date-fns";
+import { getDaysInYear, getDayOfYear, getYear } from "date-fns";
+import { Flex, Box, Text, Progress } from "@chakra-ui/react";
 
 const getYearPercentage = (partialValue, totalValue) => {
   return Math.round((100 * partialValue) / totalValue);
@@ -9,8 +10,33 @@ const App = () => {
   const today = new Date();
   const daysInYear = getDaysInYear(today);
   const curretDayOfYear = getDayOfYear(today);
+  const yearPercentage = getYearPercentage(curretDayOfYear, daysInYear);
+  const currentYear = getYear(today);
 
-  return <div>App</div>;
+  return (
+    <Flex minHeight="100vh" justify="center" align="center">
+      <Box textAlign="center">
+        <Text color="gray.900" fontStyle="oblique" fontSize="lg" mb="8px">
+          "The two most powerful warriors are patience and time."
+        </Text>
+        <Text fontSize="md" mb="42px">
+          Leo Tolstoy, War and Peace
+        </Text>
+        <Progress
+          colorScheme="blue"
+          value={yearPercentage}
+          hasStripe
+          isAnimated
+          maxValue={100}
+        />
+        <Text
+          mt="8px"
+          fontSize="sm"
+          fontWeight="bold"
+        >{`${yearPercentage}% of ${currentYear}.`}</Text>
+      </Box>
+    </Flex>
+  );
 };
 
 export default App;
