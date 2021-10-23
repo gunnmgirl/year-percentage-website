b0f003855b20cf009c381ae1441dbb971a1a92ef
Dzana <gunnmgirl@gmail.com>
2021-10-24 01:22:54 +0200

    YPW Adds year percentage progress bar compo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        margin: 0,
+        padding: 0,
+      },
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
