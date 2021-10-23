c6f1420e9f5b47c85bd6459cea344eb8402c9bc7
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2021-10-24 00:47:09 +0200

    YPW Adds get year percentage function (#2)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
+import React from "react";
+import { getDaysInYear, getDayOfYear } from "date-fns";
+
+const getYearPercentage = (partialValue, totalValue) => {
+  return Math.round((100 * partialValue) / totalValue);
+};
+
+const App = () => {
+  const today = new Date();
+  const daysInYear = getDaysInYear(today);
+  const curretDayOfYear = getDayOfYear(today);
+
   return <div>App</div>;
-}
+};
 
 export default App;
